81c6ea4ec742835f72a76c14604f82ce864abb96
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-02-06 01:33:47 +0200

    pusing Authoers

---
+++ test.py
@@ -0,0 +1,16 @@
+import cmd
+
+class HelloWorld(cmd.Cmd):
+    """Simple command processor example."""
+    
+    def do_greet(self, line):
+        print("hello")
+    
+    def do_EOF(self, line):
+        return True
+
+    def help_EOF(self):
+        print("exiting the cmd loop")
+
+if __name__ == '__main__':
+    HelloWorld().cmdloop()
