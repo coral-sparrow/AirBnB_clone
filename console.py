93e3795ad5ec1cc8a70d4fac3732978c5e3dba40
Mahmoud Gomaa <141342087+coral-sparrow@users.noreply.github.com>
2024-02-12 08:32:43 +0000

    console.py

--- console.py
+++ console.py
@@ -11,13 +11,14 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
+    def do_EOF(self, line):
+        '''exit the cmd loop'''
+        return True
+
     def do_quit(self, line):
         '''Quit command to exit the program'''
         return True
 
-    def do_EOF(self, line):
-        '''exit the cmd loop'''
-        return True
 
     def help_EOF(self):
         '''help the EOF command'''
