18e51979291c14df85e3308bcfa62493dd955154
Mahmoud Gomaa <141342087+coral-sparrow@users.noreply.github.com>
2024-02-12 08:21:11 +0000

    cosole.py pycodestyle

--- console.py
+++ console.py
@@ -1,26 +1,28 @@
 '''
-    implementing the console class 
+    implementing the console class
 '''
 
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """  Simple command processor.  """
 
     prompt = '(hbnb) '
-     
+
     def do_EOF(self, line):
         return True
-    
+
     def do_quit(self, line):
         return True
 
     def help_EOF(self):
         print("exit the cmd loop")
-    
+
     def help_quit(self):
         print("exit the cmd loop")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
